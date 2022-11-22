c1084d310006374e67fae04ebe8b5dc6f0bea263
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2022-11-22 11:40:28 +0000

    R outline for SA practical

---
+++ R/SA_practical.R
@@ -0,0 +1,33 @@
+
+# sensitivity analysis practical
+# for a cost-effectiveness analysis
+# using a discrete time Markov model
+
+
+
+## deterministic sensitivity analysis
+
+# one-way
+
+# multi-way
+
+
+# tornado plot
+
+
+## probabilistic sensitivity analysis (PSA)
+
+# pre-computing inputs
+
+
+
+# sampling during run-time
+
+
+# cost-effectiveness plane
+
+
+# cost-effectiveness acceptability curve
+
+
+
